2f1c4733d827cd78c6cf44118587ed173a14bfaf
Vghatala <Vghatala@gantner.com>
2022-12-22 15:26:20 +0530

    Sample Test -2

--- kata-dotnet-core-start.Lib.Tests/UnitTest1.cs
+++ kata-dotnet-core-start.Lib.Tests/UnitTest1.cs
@@ -11,9 +11,19 @@ public class UnitTest1
         // Assert
         Assert.Equal(output , "1");
     }
+    
+    [Fact]
+    public void Output_ShouldBe_SameAsInput_2()
+    {
+        // Act
+        var output = FizzBuzz(2);
+
+        // Assert
+        Assert.Equal(output , "2");
+    }
 
     private string FizzBuzz(int i)
     {
-        return "1";
+        return $"{i}";
     }
 }
